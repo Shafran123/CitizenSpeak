6ac20ba0c2c52287471e5c12d7240e86fa1582d5
Shafran <Shafran@ieee.org>
2020-02-08 12:50:09 +0530

    ejected from native base

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-
 import reducers from './src/reducers'
 
+import {StyleProvider} from 'native-base';
+import commonColors from './native-base-theme/variables/commonColor'
+import getTheme from './native-base-theme/components'
 import AppNavigator from './src/Components/Navigation/AppNavigator'
 
 class App extends React.Component {
@@ -15,8 +17,9 @@ class App extends React.Component {
     return (
 
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <AppNavigator />
-
+          <StyleProvider style={getTheme(commonColors)}>
+            <AppNavigator />
+          </StyleProvider>
       </Provider>
 
 
